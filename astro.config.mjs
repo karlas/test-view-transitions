732e0c1558b954ecd1029ad67ed6508cbbeaef3c
Carles <carles.pages.rozas@gmail.com>
2024-03-07 22:15:32 +0100

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import devtoolbarTailwind from "astro-devtoolbar-tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), devtoolbarTailwind()],
+  prefetch: true
+});
